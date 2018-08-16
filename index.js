b1413399bcf76b110208fa3c490bc83c5dfe69d7
vaeb80@gmail.com Echeveste <vaeb80@gmail.com>
2018-08-16 20:30:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function logWhisper(string){
 }
 function sayHiToGrandma(string){
   if (string === "I love you, Grandma"){
-    return "I love you, too."
+    return 'I love you, too.'
   }
 }
 /*"I can\'t hear you!" if `string` is lowercase'
