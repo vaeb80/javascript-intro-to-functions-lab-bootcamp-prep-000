847af52a9777442d434e2698a58493d4be3f91c8
vaeb80@gmail.com Echeveste <vaeb80@gmail.com>
2018-08-16 20:31:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function logShout(string){
 function logWhisper(string){
   return console.log(string.toLowerCase())
 }
-function sayHiToGrandma(string){
+function sayHiToGrandma(){
   if (string === "I love you, Grandma"){
     return 'I love you, too.'
   }
