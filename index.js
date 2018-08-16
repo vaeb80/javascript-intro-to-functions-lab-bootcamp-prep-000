65447778832122188175cfab254bd327d4b10136
vaeb80@gmail.com Echeveste <vaeb80@gmail.com>
2018-08-16 20:20:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ function whisper(string){
 function logShout(string){
   return console.log(string.toUpperCase())
 }
+function logWhisper(string){
+  return console.log(string.toLowerCase())
+}
 /*
 function sayHiToGrandma(string){
   
