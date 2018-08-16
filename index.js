4d33d4d4d5b993b430016a83d8a531eb63643a21
vaeb80@gmail.com Echeveste <vaeb80@gmail.com>
2018-08-16 20:17:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 function shout(string){
   return string.toUpperCase()
 }
+function whisper(string){
+  return string.toLowerCase()
+}
+function logShout(string){
+  return console.log()
+}
+
+function sayHiToGrandma(string){
+  
+"I can\'t hear you!" if `string` is lowercase'
+  
+"YES INDEED!" if `string` is uppercase', 
+
+"I love you, too." if `string` is "I love you, Grandma."
+}
